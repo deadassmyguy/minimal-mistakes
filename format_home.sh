441f8fb86f3c61999d5a4a07e9814fe7158b9198
Abdul Rabbani <58230246+abdulrabbani00@users.noreply.github.com>
2024-11-11 20:11:27 -0500

    Minor updates

--- format_home.sh
+++ format_home.sh
@@ -12,10 +12,9 @@ fi
 pandoc -f markdown -t html index.md -o index.html
 
 # Linux
-#sed -i '1s/^/---\ntitle: "Hello, World"\nlayout: home\nauthor_profile: true\nheader:\n  overlay_image: "assets\/images\/home-cover-1.jpg"\n---\n\n/' index.html
+sed -i '1s/^/---\ntitle: "Hello, World"\nlayout: home\nauthor_profile: true\nheader:\n  overlay_image: "assets\/images\/home-cover-1.jpg"\n---\n\n/' index.html
 
 # Mac
-sed -i '.original' -e '1s/^/---\ntitle: "Hello, World"\nlayout: home\nauthor_profile: true\nheader:\n  overlay_image: "assets\/images\/home-cover-1.jpg"\n---\n\n/' index.html
+#sed -i '.original' -e '1s/^/---\ntitle: "Hello, World"\nlayout: home\nauthor_profile: true\nheader:\n  overlay_image: "assets\/images\/home-cover-1.jpg"\n---\n\n/' index.html
 
 mv index.md _index.md
-rm index.html.original
